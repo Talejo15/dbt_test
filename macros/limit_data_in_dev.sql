89525e7e8a57964d9a85955becba71ca62e27ba0
Toheeb Alejo <toheeb.r.alejo@gmail.com>
2022-12-29 21:07:13 +0000

    adding packages

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,4 +1,4 @@
-{%  macro limit_data_in_dev(column_name, dev_years_of_data)   %}
+{%  macro limit_data_in_dev(column_name, dev_years_of_data=5)   %}
 {% if target.name == 'default'   %}
 where {{column_name}} >= date_sub(current_date(), interval {{dev_years_of_data}} year)
 {% endif   %}
